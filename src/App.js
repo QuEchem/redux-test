cf8d60b76b330d0ec0f98f6a372982b6b635c892
Nnaemeka Echem <echem2011@hotmail.com>
2020-10-10 00:13:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {createStore} from "redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">Redux test</div>
+    );
+  }
 }
 
 export default App;
+
+const defaultState = {
+  welcome: "Hi",
+  otherState: "some stuff"
+}
+
+const greetings = (state = defaultState, action) => {
+  switch(action.type){
+    case "GREET_ME":
+      return {...state, welcome: `Hello ${action.result}`}
+    case "GREET_WORLD":
+      return {...state, welcome: "Hello World"}
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(greetings)
+
+const result = "Some random API"
+store.dispatch({
+  type: "GREET_ME",
+  result
+})
+
+
+console.log(store.getState())
